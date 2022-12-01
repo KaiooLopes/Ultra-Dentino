6949bbea327eb8f6fa0997c0fec4b78e314e8873
Kaio Lopes <kaio.loopes18@gmail.com>
2022-12-01 14:56:16 -0300

    Adicionando Fonte

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ import ReferenceSection from "./components/ReferenceSection";
 import SatisfactionSection from "./components/SatifactionSection";
 import UltraDentinoSection from "./components/UltraDentinoSection";
 import WhyCreatedSection from "./components/WhyCreatedSection";
+import Font from "./fonts/Quicksand-VariableFont_wght.ttf";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "QuickSand" ;
+    src: url(${Font});
+  }
+
   *{
     margin: 0;
     padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, 
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "QuickSand", sans-serif;
   }
 `;
 
