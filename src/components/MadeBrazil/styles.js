dd889324404c5e6c76815a714339ffd016b7d611
octalad <ladeiraoctavio@gmail.com>
2022-12-01 16:37:42 -0300

    MadeBrazil finalizado

--- src/components/MadeBrazil/styles.js
+++ src/components/MadeBrazil/styles.js
@@ -4,10 +4,15 @@ export const MadeBrazilContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
   background-color: #00005c;
-  gap: 100px;
-  padding: 20px 300px 20px 150px;
+  gap: 20px;
+  padding: 20px 0px;
+  @media (min-width: 992px) {
+    justify-content: space-around;
+    gap: 100px;
+    padding: 20px 300px 20px 150px;
+  }
 `;
 
 export const MadeBrazilImg = styled.img`
