8fd0fa7e2d265345723be68c4846722ba5bd68ca
octalad <ladeiraoctavio@gmail.com>
2022-12-16 17:48:59 -0300

    refund starter

--- src/pages/RefundPolicy/index.js
+++ src/pages/RefundPolicy/index.js
@@ -1,10 +1,47 @@
 import MultiPagesNav from "../../components/MultiPagesNav";
-import { RefundPolicyContainer } from "./styles";
+import {
+  RefundIntro,
+  RefundIntroText,
+  RefundIntroTextDiv,
+  RefundIntroTitle,
+  RefundPolicyContainer,
+} from "./styles";
 
 const RefundPolicy = () => {
   return (
     <RefundPolicyContainer>
-      <h1>RefundPolicy</h1>
+      <RefundIntro>
+        <RefundIntroTitle>
+          <b>UltraDentino</b> <br /> <small>Política de envío</small>
+        </RefundIntroTitle>
+        <RefundIntroTextDiv>
+          <RefundIntroText>
+            When you decide to purchase ProDentim or related products, your
+            order will be automatically shipped after you've filled in the order
+            form and your payment is confirmed.
+          </RefundIntroText>
+          <RefundIntroText>
+            In no more than 60 hours, you will receive an email with your
+            shipping tracking ID and a personalized link that allows you to
+            check your shipment anytime you like.
+          </RefundIntroText>
+          <RefundIntroText>
+            After receiving this email, you can rest assured, as your order is
+            on its way to you and will be delivered to the address you have
+            provided us as soon as possible, as our trusted shipping partner
+            guarantees.
+          </RefundIntroText>
+          <RefundIntroText>
+            We'll make sure that you only receive premium quality products, as
+            your health and satisfaction with our supplements is our goal and
+            business identity. If, at any time, you have any questions, simply
+            let us know by writing us an email to this address
+            <b>
+              <a> contact@prodentim-product.com.</a>
+            </b>
+          </RefundIntroText>
+        </RefundIntroTextDiv>
+      </RefundIntro>
       <MultiPagesNav />
     </RefundPolicyContainer>
   );
--- src/pages/RefundPolicy/styles.js
+++ src/pages/RefundPolicy/styles.js
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const RefundPolicyContainer = styled.div``;
+export const RefundPolicyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 24px;
+`;
+
+export const RefundIntro = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  text-align: start;
+`;
+
+export const RefundIntroTitle = styled.h2`
+  font-size: 32px;
+  font-weight: 400;
+  padding: 24px 4px;
+  text-align: start;
+`;
+
+export const RefundIntroTextDiv = styled.div`
+  gap: 16px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 0px;
+`;
+
+export const RefundIntroText = styled.p`
+  font-size: 20px;
+  font-weight: 200;
+`;
